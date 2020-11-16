479bf66d56a36471380d535e20410152730cb1b7
kavi4 <antoniy986@gmail.com>
2020-11-16 15:38:19 +0300

    add time measure units

---
+++ src/time/coefficients.js
@@ -0,0 +1,17 @@
+import Units from './units'
+
+export default [
+    [Units.DECADE, 315360000],
+    [Units.YEAR, 31536000],
+    [Units.SEMESTER, 15552000],
+    [Units.QUARTER, 7776000],
+    [Units.MONTH, 2592000],
+    [Units.TEN_DAY_PERIOD, 864000],
+    [Units.WEEK, 604800],
+    [Units.DAYS, 86400],
+    [Units.HOUR, 3600],
+    [Units.MINUTE, 60],
+    [Units.SECOND, 1],
+    [Units.MILLISECOND, 0.001],
+    [Units.MICROSECONDS, 0.000001],
+]
