d8a3f8ab4960569719360c159e4723b53b047e33
kavi4 <antoniy986@gmail.com>
2020-11-16 15:37:34 +0300

    add area measure units

---
+++ src/area/units.js
@@ -0,0 +1,36 @@
+export default {
+    //*** Международные единицы измерения ***
+    SQUARE_MILLIMETER: '050',//Квадратный миллиметр
+    SQUARE_CENTIMETER: '051',//Квадратный сантиметр
+    SQUARE_DECIMETER: '053',//Квадратный дециметр
+    SQUARE_METER: '055',//Квадратный метр
+    THOUSAND_SQUARE_METERS: '058',//Тысяча квадратных метров
+    HECTARE: '059',//Гектар
+    SQUARE_KILOMETER: '061',//Квадратный километр
+    SQUARE_INCH: '071',//Квадратный дюйм (645,16 мм2)
+    SQUARE_FEET: '073',//Квадратный фут (0,092903 м2)
+    SQUARE_YARD: '075',//Квадратный ярд (0,8361274 м2)
+    ARE: '109',//Ар (100 м2)
+
+    //*** Национальные единицы измерения ***
+    THOUSAND_SQUARE_DECIMETERS: '054',// Тысяча квадратных дециметров
+    MILLION_SQUARE_DECIMETERS: '056',// Миллион квадратных дециметров
+    MILLION_SQUARE_METERS: '057',// Миллион квадратных метров
+    THOUSAND_HECTARES: '060',// Тысяча гектаров
+    CONDITIONAL_SQUARE_METER: '062',// Условный квадратный метр
+    THOUSAND_CONDITIONAL_SQUARE_METERS: '063',// Тысяча условных квадратных метров
+    MILLION_CONDITIONAL_SQUARE_METERS: '064',// Миллион условных квадратных метров
+    SQUARE_METER_TOTAL_AREA: '081',// Квадратный метр обшей площади
+    THOUSAND_SQUARE_METERS_OF_TOTAL_AREA: '082',// Тысяча квадратных метров общей площади
+    MILLION_SQUARE_METERS_TOTAL_AREA: '083',// Миллион квадратных метров общей площади
+    SQUARE_METER_LIVING_SPACE: '084',// Квадратный метр жилой площади
+    THOUSAND_SQUARE_METERS_OF_LIVING_SPACE: '085',// Тысяча квадратных метров жилой площади
+    MILLION_SQUARE_METERS_OF_LIVING_SPACE: '086',// Миллион квадратных метров жилой площади
+    SQUARE_METER_OF_EDUCATIONAL_AND_LABORATORY_BUILDINGS: '087',// Квадратный метр учебно-лабораторных зданий
+    THOUSAND_SQUARE_METERS_OF_EDUCATIONAL_AND_LABORATORY_BUILDINGS: '088',// Тысяча квадратных метров учебно-лабораторных зданий
+    MILLION_SQUARE_METERS_IN_TWO_MILLIMETERS: '089',// Миллион квадратных метров в двухмиллиметровом исчислении
+
+    //*** Международные единицы измерения, не включенные в ОКЕИ ***
+    ACRES: '077',// Акр (4840 квадратных ярдов)
+    SQUARE_MILE: '079',// Квадратная миля
+}
