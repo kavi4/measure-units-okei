e2fd8ca6103f5aefad3e46bea35d36a42b8afd26
kavi4 <antoniy986@gmail.com>
2020-11-16 15:37:57 +0300

    add length measure units

---
+++ src/length/coefficients.js
@@ -0,0 +1,21 @@
+import Units from './units'
+
+export default [
+    [Units.MEGAMETER, 1000000],
+    [Units.NAUTICAL_MILE, 1852],
+    [Units.MILE_CHARTER, 1609.344],
+    [Units.KILOMETER, 1000],
+    [Units.THOUSAND_RUNNING_METERS, 1000],
+    [Units.THOUSAND_NOMINAL_METERS, 1000],
+    [Units.KILOMETER_NOMINAL_PIPES, 1000],
+    [Units.HECTOMETER, 100],
+    [Units.METER, 1],
+    [Units.RUNNING_METER, 1],
+    [Units.NOMINAL_METER, 1],
+    [Units.YARD, 0.9144],
+    [Units.DECIMETER, 0.1],
+    [Units.FEET, 0.3048],
+    [Units.INCH, 0.0254],
+    [Units.CENTIMETER, 0.01],
+    [Units.MILLIMETER, 0.001],
+]
